d0b70ade5e88032556d8777219894f7374a55682
Danny Rivera <dhriveradev@gmail.com>
2022-02-22 17:13:45 -0600

    endpoint name typo fixed

--- routes/auth.ts
+++ routes/auth.ts
@@ -4,7 +4,7 @@ import { login, register } from '../controller/auth';
 
 const router = Router();
 
-router.post('/register', register);
+router.post('/signup', register);
 router.post('/login', login);
 
 export default router;
